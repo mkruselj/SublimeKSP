f79a457da591b57d8ffe25b983c57f6b6c977aa8
Nabeel Ansari <nabeelansarillc@gmail.com>
2024-05-12 16:47:59 -0400

    Updated IVLS snippet to use new inherited playevent style

--- compiler/ivls.py
+++ compiler/ivls.py
@@ -12,10 +12,10 @@ def ivls_pre_analyze(line_obj_list):
         
         if line.startswith("vo_field"):
             import re
-            match = re.match(r"vo_field\s+(\w+)\s*=\s*(\d+)\s+if\s+(.+)", line)
+            match = re.match(r"vo_field\s+(\w+)\s*=\s*(\w+)\s+if\s+(.+)", line)
             
             if not (match and match.group(1) and match.group(2) and match.group(3)):
-                raise ParseException(Line(line, [None, 0], None), 'Field declarations must be in form "vo_field \'name\' = \'default value\' if (\'boolean expression\')!')
+                raise ParseException(Line(line, [(None, 0)], None), 'Field declarations must be in form "vo_field \'name\' = \'default value\' if (\'boolean expression\')!')
             
             var_name = match.group(1)
             value = match.group(2)
