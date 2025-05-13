288bae02a37d3d5708dfb41d873510f97e66eee0
Mario Kruselj <mario.kruselj@gmail.com>
2025-05-13 14:49:36 +0200

    Fix build-vsix.bat (didn't call npm compile/package)

--- build-vsix.bat
+++ build-vsix.bat
@@ -1,6 +1,6 @@
 echo "[Build SublimeKSP for VS Code] Converting .sublime-snippets to VS Code snippets..."
 call npx snippetToVsCode -s "snippets" -o "vscode/snippets.json"
 echo "[Build SublimeKSP for VS Code] Transpiling extension entry point from TypeScript to JavaScript..."
-npm run compile
+call npm run compile
 echo "[Build SublimeKSP for VS Code] Building extension..."
-npm run package
+call npm run package
